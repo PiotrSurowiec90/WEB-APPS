1d34193069e7f71fe54eda4d2e9acf20000c355e
Piotr Surowiec <piotrek.scgn@gmail.com>
2023-10-13 11:49:23 +0100

    Completed 03 debuggin chellange

---
+++ Phase-03/postcode_checker_app/lib/postcode_checker.py
@@ -0,0 +1,10 @@
+import re
+
+class PostcodeChecker():
+    def check(self, postcode):
+        if postcode is None:
+            return False
+        return re.match(
+            r"^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$",
+            postcode,
+            re.IGNORECASE) is not None
